a5c6e00d0c76d4039185cd29e7691176aa12023e
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2021-09-20 01:47:26 -0300

    Update for TRON

--- executeAGminer.py
+++ executeAGminer.py
@@ -2,10 +2,12 @@
 from os import system
 from time import sleep
 
+wall = "TPidhtNgpDDouvgE6unrLzsAbFBZW45CTj"
+
 try:
-	system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz && tar -xvf lolMiner_v1.32a_Lin64.tar.gz")
-	system("echo AAAA")
-	system("cd 1.32a")
-	system("./1.32a/lolMiner --algo BEAM-III  --pool  eu1-beam.flypool.org:3333 --user ab16399364798c38ab3636ffdbdad828dc7bede2f0f305cfa709635153f1a64799.MinerMiq --tls 0")
+		system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.32/lolMiner_v1.32a_Lin64.tar.gz && tar -xvf lolMiner_v1.32a_Lin64.tar.gz")
+		system("echo AAAA")
+		system("cd 1.32a")
+		system("./1.32a/lolMiner --algo ethash --pool  ethash.unmineable.com:3333 --user TRX:{}.MinerMiq --tls 0".format(str(wall)))
 except Exception as err:
 		print(" Error: ", err)
